add94473e8f9af6c275899efb9c8d9cb4b8602ac
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-07-09 22:45:03 -0400

    fix emptyresult, it will now make another request

--- app.js
+++ app.js
@@ -16,30 +16,47 @@ const url = nonExactUrl.concat(exactUrl);
 
 app.use(express.static(resolve(__dirname, "public")));
 
+let text = [];
 app.get("/items", (req, res, next) => {
-  let text = [];
-
-  const requests = url.map((item, itemIdx) => {
-    return new Promise(resolveCb => {
-      request(item, (err, response, body) => {
-        if (err) {
-          text[itemIdx] = err;
-        } else {
-          const listMarket = new JSDOM(body).window.document.getElementById("listMarket");
-          listMarket.getElementsByTagName("tbody")[0].className = "itemTable";
-          listMarket.getElementsByTagName("tr")[0].className = "show";
-          text[itemIdx] = listMarket.innerHTML;
-        }
-        resolveCb();
+  if (text.length) {
+    res.send(text);
+  } else {
+    const requests = url.map((item, itemIdx) => {
+      return new Promise(resolveCb => {
+        newRequest(item, itemIdx, resolveCb);
       });
     });
-  });
 
-  Promise.all(requests)
-    .then(() => {
-      res.send(text);
-    });
+    Promise.all(requests)
+      .then(() => {
+        res.send(text);
+        setTimeout(() => { text = []; }, 5000);
+      })
+      .catch((err) => {
+        setTimeout(() => { text = []; }, 5000);
+        next(err);
+      });
+  }
 });
+
+function newRequest(item, itemIdx, resolveCb) {
+  request(item, (err, response, body) => {
+    if (err) {
+      text[itemIdx] = err;
+      resolveCb();
+    } else {
+      const listMarket = new JSDOM(body).window.document.getElementById("listMarket");
+      listMarket.getElementsByTagName("tbody")[0].className = "itemTable";
+      listMarket.getElementsByTagName("tr")[0].className = "show";
+      if (listMarket.getElementsByClassName("emptyResult").length) {
+        newRequest(item, itemIdx, resolveCb);
+      } else {
+        text[itemIdx] = listMarket.innerHTML;
+        resolveCb();
+      }
+    }
+  });
+}
 
 const port = process.env.PORT || 3000;
 
