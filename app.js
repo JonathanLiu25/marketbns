b827a7ddc8047eb787fac4f0ba415ef310fae398
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-07-18 14:56:07 -0400

    add sequelize and items table

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const { resolve } = require("path");
+const models = require("./models");
 
 const app = express();
 
-app.use("/items", require("./routes"));
+app.use("/items", require("./routes.js"));
 
 app.use(express.static(resolve(__dirname, "public")));
+
 
 const port = process.env.PORT || 3000;
 
 if (module === require.main) {
+  const force = true;
+  models.db.sync({ force });
+
   const server = app.listen(port, () => {
     console.log("Listening on port", server.address().port);
   });
--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
+const db = require("./db.js");
 const Items = require("./Items.js");
 
 module.exports = {
+  db,
   Items
 };
