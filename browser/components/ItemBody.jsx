23c447519004c3e74c366648ea59f572a1a37a23
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-07-26 17:37:38 -0400

    refactor and remove unnecessary lines

--- browser/components/ItemBody.jsx
+++ browser/components/ItemBody.jsx
@@ -4,11 +4,8 @@ import ItemRow from "./ItemRow.jsx";
 const ItemBody = props => (
   <tbody>
     {props.items.length && props.items.map((item, itemIdx) => {
-      if (props.showAll) item.show = true;
-      if (itemIdx === 0) item.show = true;
-      return (<ItemRow
-        item={item}
-        key={`item-row-${itemIdx}`} />);
+      if (props.showAll || itemIdx === 0) item.show = true;
+      return <ItemRow item={item} key={`item-row-${itemIdx}`} />;
     })}
   </tbody>
 );
